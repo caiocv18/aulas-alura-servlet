26ea4700ba20a8d79e9e3e2180be3d84e573b1f3
Caio Vinicius <caioviniciuscv18@gmail.com>
2020-11-23 04:31:36 -0300

    Aula 4 - adicionando páginas JSP

--- src/br/com/caelum/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/caelum/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,8 +1,8 @@
 package br.com.caelum.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,23 +14,23 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Cadastrando nova empresa");
-		
-		String novaEmpresa = request.getParameter("nome");
-		Empresa empresa = new Empresa();
-		empresa.setNome(novaEmpresa);
-		
-		Banco banco = new Banco();
-		banco.adiciona(empresa);
-		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body> Empresa " + novaEmpresa + " cadastrada com sucesso!</body></html>");
-	}
 
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        System.out.println("Cadastrando nova empresa");
+
+        String nomeEmpresa = request.getParameter("nome");
+        Empresa empresa = new Empresa();
+        empresa.setNome(nomeEmpresa);
+
+        Banco banco = new Banco();
+        banco.adiciona(empresa);
+
+        //chamar a JSP
+        RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+        request.setAttribute("empresa", empresa.getNome());
+        rd.forward(request, response);
+    }
 }
